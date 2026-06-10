a52fe0fb7a8d496499e03aafdf92bf9b23b938ad
Arthur Sales Valdivino <arthursalesv10@gmail.com>
2026-06-10 18:28:52 +0000

    commit 1

---
+++ Index.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once 'Model.php';
+require_once 'View.php';
+require_once 'Controller.php';
+
+// Criar instâncias
+
+$model = new Model();
+
+$controller = new Controller($model);
+
+$view = new View($model);
+
+// Verifica a ação e executa
+
+if (isset($_GET['action'])) {
+    switch ($_GET['action']) {
+
+        case 'somar':
+            $controller->somar();
+            break;
+
+        case 'subtrair':
+            $controller->subtrair();
+            break;
+
+        case 'multiplicar':
+            $controller->multiplicar();
+            case 'dividir':
+                $controller->dividir();
+                break;
+            }
+        }
+
+    //exibe na tela
+    echo $view-> output();
